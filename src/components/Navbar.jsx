735219d245407659ecae3b46831b9e35183883dd
Brad Traversy <traversymedia@gmail.com>
2021-11-19 10:44:44 -0500

    Navbar == warning fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   const location = useLocation()
 
   const pathMatchRoute = (route) => {
-    if (route == location.pathname) {
+    if (route === location.pathname) {
       return true
     }
   }
