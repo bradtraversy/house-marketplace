0ff776980d9742bcacd9cb3f7635484cb08e85b0
Brad Traversy <traversymedia@gmail.com>
2021-11-24 09:14:33 -0500

    Fixed location issue

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
 import CreateListing from './pages/CreateListing'
+import Listing from './pages/Listing'
 
 function App() {
   return (
@@ -27,6 +28,10 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/category/:categoryName/:listingId'
+            element={<Listing />}
+          />
         </Routes>
         <Navbar />
       </Router>
--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -112,7 +112,6 @@ function CreateListing() {
     } else {
       geolocation.lat = latitude
       geolocation.lng = longitude
-      location = address
     }
 
     // Store image in firebase
@@ -169,9 +168,9 @@ function CreateListing() {
       timestamp: serverTimestamp(),
     }
 
+    formDataCopy.location = address
     delete formDataCopy.images
     delete formDataCopy.address
-    location && (formDataCopy.location = location)
     !formDataCopy.offer && delete formDataCopy.discountedPrice
 
     const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
