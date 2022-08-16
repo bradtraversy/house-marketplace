e07f2c5e48980d12f0bb84b0e7d702494664cfa0
Sai Nath <92171156+sai-na@users.noreply.github.com>
2022-08-17 03:30:31 +0530

    input  type fixed
    line 142   ,
      input  type="text" changed into   type='email'

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -139,7 +139,7 @@ function Profile() {
               onChange={onChange}
             />
             <input
-              type='text'
+              type='email'
               id='email'
               className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
               disabled={!changeDetails}
