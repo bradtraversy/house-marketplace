5b90388dddd7ccd02b8b74f183598281a4526fb5
xdangos_dev <47195962+abdallamfathy@users.noreply.github.com>
2022-10-15 14:24:09 +0200

    use docRef instead of call table again

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -22,7 +22,7 @@ function OAuth() {
 
       // If user, doesn't exist, create user
       if (!docSnap.exists()) {
-        await setDoc(doc(docRef), {
+        await setDoc(docRef), {
           name: user.displayName,
           email: user.email,
           timestamp: serverTimestamp(),
